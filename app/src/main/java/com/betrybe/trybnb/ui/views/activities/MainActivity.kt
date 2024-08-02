16e2a8041f4b9d6850db0eab9b35c003714f292f
Julio Matias <juliocesark2@hotmail.com>
2024-08-02 02:54:33 -0300

    feat: implements fragments and click event listener by bottomNavigation in the app - req 03

--- app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
@@ -2,12 +2,43 @@ package com.betrybe.trybnb.ui.views.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.betrybe.trybnb.R
+import com.betrybe.trybnb.ui.views.fragments.CreateReservationFragment
+import com.betrybe.trybnb.ui.views.fragments.ProfileFragment
+import com.betrybe.trybnb.ui.views.fragments.ReservationFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val bottomNavigation: BottomNavigationView by lazy { findViewById(R.id.navigation_bottom_view) }
+    private val reservationFragment = ReservationFragment()
+    private val createReservationFragment = CreateReservationFragment()
+    private val profileFragment = ProfileFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.reservation_menu_item -> {
+                    changeFragmentMenu(reservationFragment)
+                }
+                R.id.create_reservation_menu_item -> {
+                    changeFragmentMenu(createReservationFragment)
+                }
+                R.id.profile_menu_tem -> {
+                    changeFragmentMenu(profileFragment)
+                }
+            }
+            true
+        }
+    }
+
+    private fun changeFragmentMenu(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fragment_container, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ReservationFragment.kt
@@ -0,0 +1,20 @@
+package com.betrybe.trybnb.ui.views.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.betrybe.trybnb.R
+
+class ReservationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reservation, container, false)
+    }
+
+}
