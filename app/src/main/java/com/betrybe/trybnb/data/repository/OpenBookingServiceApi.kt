af63cefe1207fbb187b6bcc7925d615ee20d9f61
Julio Matias <juliocesark2@hotmail.com>
2024-08-07 16:59:40 -0300

    fix: response getBookingIds for list of BookingId and headers in getBookingById

--- app/src/main/java/com/betrybe/trybnb/data/repository/OpenBookingServiceApi.kt
+++ app/src/main/java/com/betrybe/trybnb/data/repository/OpenBookingServiceApi.kt
@@ -2,13 +2,14 @@ package com.betrybe.trybnb.data.repository
 
 import com.betrybe.trybnb.data.models.AuthRequest
 import com.betrybe.trybnb.data.models.BookingData
-import com.betrybe.trybnb.data.models.BookingIdsData
+import com.betrybe.trybnb.data.models.BookingId
 import com.betrybe.trybnb.data.models.TokenData
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.Path
 
 interface OpenBookingServiceApi {
     @POST("auth")
@@ -17,10 +18,11 @@ interface OpenBookingServiceApi {
     ) : Response<TokenData>
 
     @GET("booking")
-    suspend fun getBookingIds() : Response<BookingIdsData>
+    suspend fun getBookingIds() : Response<List<BookingId>>
 
+    @Headers ("Content-Type: application/json", "Accept: application/json")
     @GET("booking/{id}")
     suspend fun getBookingById(
-        @Query("id") id: Int
+        @Path("id") id: String
     ) : Response<BookingData>
 }
