574ff51550ebbc6d5c4608b1ea74723a01df22e5
Julio Matias <juliocesark2@hotmail.com>
2024-08-07 02:30:57 -0300

    feat: implements request retrofit routes, booking and booking/:id in OpenBookingServiceApi

--- app/src/main/java/com/betrybe/trybnb/data/repository/OpenBookingServiceApi.kt
+++ app/src/main/java/com/betrybe/trybnb/data/repository/OpenBookingServiceApi.kt
@@ -1,14 +1,26 @@
 package com.betrybe.trybnb.data.repository
 
 import com.betrybe.trybnb.data.models.AuthRequest
+import com.betrybe.trybnb.data.models.BookingData
+import com.betrybe.trybnb.data.models.BookingIdsData
 import com.betrybe.trybnb.data.models.TokenData
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface OpenBookingServiceApi {
     @POST("auth")
     suspend fun getAuth(
         @Body body: AuthRequest
     ) : Response<TokenData>
+
+    @GET("booking")
+    suspend fun getBookingIds() : Response<BookingIdsData>
+
+    @GET("booking/{id}")
+    suspend fun getBookingById(
+        @Query("id") id: Int
+    ) : Response<BookingData>
 }
