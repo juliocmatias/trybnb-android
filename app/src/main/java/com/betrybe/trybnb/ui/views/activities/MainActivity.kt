b20d25a7109badd7798886ce7867db3f599c1b81
Julio Matias <juliocesark2@hotmail.com>
2024-08-17 12:18:22 -0300

    refactor: implement binding in the MainActivity

--- app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.betrybe.trybnb.R
+import com.betrybe.trybnb.databinding.ActivityMainBinding
 import com.betrybe.trybnb.ui.views.fragments.CreateReservationFragment
 import com.betrybe.trybnb.ui.views.fragments.ProfileFragment
 import com.betrybe.trybnb.ui.views.fragments.ReservationFragment
@@ -11,14 +12,17 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private val bottomNavigation: BottomNavigationView by lazy { findViewById(R.id.navigation_bottom_view) }
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigation: BottomNavigationView
     private val reservationFragment = ReservationFragment()
     private val createReservationFragment = CreateReservationFragment()
     private val profileFragment = ProfileFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        bottomNavigation = binding.navigationBottomView
 
         changeFragmentMenu(profileFragment)
         bottomNavigation.selectedItemId = R.id.profile_menu_tem
