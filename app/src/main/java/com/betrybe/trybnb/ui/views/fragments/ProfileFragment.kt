79430c7c9df9778c7b16b85ca681b6c1001c3a65
Julio Matias <juliocesark2@hotmail.com>
2024-08-19 21:58:13 -0300

    refactor: ProfileFragment implements binding

--- app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ProfileFragment.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/fragments/ProfileFragment.kt
@@ -5,13 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.FrameLayout
 import androidx.core.widget.addTextChangedListener
-import com.betrybe.trybnb.R
 import com.betrybe.trybnb.common.ApiIdlingResource
 import com.betrybe.trybnb.data.models.AuthRequest
 import com.betrybe.trybnb.data.repository.OpenBookingService
+import com.betrybe.trybnb.databinding.FragmentProfileBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
 import kotlinx.coroutines.CoroutineScope
@@ -22,95 +20,86 @@ import java.io.IOException
 
 class ProfileFragment : Fragment() {
 
-    private lateinit var loginInputProfile: TextInputLayout
-    private lateinit var passwordInputProfile: TextInputLayout
-    private lateinit var loginButtonProfile: Button
-    private lateinit var waitingResponseState: FrameLayout
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     private val bookingServiceApi = OpenBookingService.instance
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        loginInputProfile = view.findViewById(R.id.login_input_profile)
-        passwordInputProfile = view.findViewById(R.id.password_input_profile)
-        loginButtonProfile = view.findViewById(R.id.login_button_profile)
-        waitingResponseState = view.findViewById(R.id.waiting_response_state)
-    }
+        setupTextInputLayoutListener(binding.loginInputProfile)
+        setupTextInputLayoutListener(binding.passwordInputProfile)
 
-    override fun onStart() {
-        super.onStart()
+        binding.loginButtonProfile.setOnClickListener {
+            val login = binding.loginInputProfile.editText?.text.toString()
+            val password = binding.passwordInputProfile.editText?.text.toString()
 
-        loginInputProfile.setEndIconOnClickListener {
-            passwordInputProfile.requestFocus()
-        }
-
-        loginInputProfile.editText?.addTextChangedListener {
-            if (it.toString().isNotEmpty()) {
-                loginInputProfile.error = null
-            }
-        }
-
-        passwordInputProfile.editText?.addTextChangedListener {
-            if (it.toString().isNotEmpty()) {
-                passwordInputProfile.error = null
-            }
-        }
-
-        loginButtonProfile.setOnClickListener {
-            val login = loginInputProfile.editText?.text.toString()
-            val password = passwordInputProfile.editText?.text.toString()
-
-            val isLoginValid = validateLoginProfile(login, password)
-
-            if (isLoginValid) {
+            if (validateLoginProfile(login, password)) {
 
                 showProgress()
 
-                CoroutineScope(Dispatchers.IO).launch {
-                    try {
-                        ApiIdlingResource.increment()
+                loginUser(login, password)
+            }
+        }
+    }
 
-                        val bodyRequest = AuthRequest(login, password)
+    private fun loginUser(login: String, password: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                ApiIdlingResource.increment()
 
-                        val response = bookingServiceApi.getAuth(bodyRequest)
+                val bodyRequest = AuthRequest(login, password)
+                val response = bookingServiceApi.getAuth(bodyRequest)
 
-                        if (response.isSuccessful) {
-                            withContext(Dispatchers.Main) {
-                                val token = response.body()?.token
+                withContext(Dispatchers.Main) {
+                    if (response.isSuccessful) {
+                        val token = response.body()?.token
 
-                                if (token !== null) {
-                                    hideProgress()
-                                    showSnack("Login feito com sucesso!")
-                                } else {
-                                    hideProgress()
-                                    showSnack("Usuário ou senha inválidos")
-                                }
-                            }
-                        } else {
-                            withContext(Dispatchers.Main) {
-                                hideProgress()
-                                showSnack("Erro ao efetuar login")
-                            }
-                        }
-
-                        ApiIdlingResource.decrement()
-                    } catch (e: IOException) {
-                        ApiIdlingResource.decrement()
-                        withContext(Dispatchers.Main) {
+                        if (token !== null) {
                             hideProgress()
-                            showSnack("Error: Falha na comunicação com o servidor")
+                            showSnack("Login feito com sucesso!")
+                        } else {
+                            hideProgress()
+                            showSnack("Usuário ou senha inválidos")
                         }
+                    } else {
+                        hideProgress()
+                        showSnack("Erro ao efetuar login")
                     }
+
+                    ApiIdlingResource.decrement()
                 }
+            } catch (e: IOException) {
+                ApiIdlingResource.decrement()
+                withContext(Dispatchers.Main) {
+                    hideProgress()
+                    showSnack("Erro: Falha na comunicação com o servidor")
+                }
+            }
+        }
+    }
+
+
+    private fun setupTextInputLayoutListener(textInputLayout: TextInputLayout) {
+        textInputLayout.editText?.addTextChangedListener {
+            if (!it.isNullOrEmpty()) {
+                textInputLayout.error = null
+            }
+        }
+        if (textInputLayout.hint == "Login") {
+            textInputLayout.setEndIconOnClickListener {
+                binding.passwordInputProfile.requestFocus()
             }
         }
     }
@@ -119,30 +108,26 @@ class ProfileFragment : Fragment() {
         var isValid = true
 
         if (login.isEmpty()) {
-            loginInputProfile.error = "O campo Login é obrigatório"
+            binding.loginInputProfile.error = "O campo Login é obrigatório"
             isValid = false
-        } else {
-            loginInputProfile.error = null
         }
 
         if (password.isEmpty()) {
-            passwordInputProfile.error = "O campo Password é obrigatório"
+            binding.passwordInputProfile.error = "O campo Password é obrigatório"
             isValid = false
-        } else {
-            passwordInputProfile.error = null
         }
 
         return isValid
     }
 
     private fun showProgress() {
-        waitingResponseState.visibility = View.VISIBLE
-        loginButtonProfile.visibility = View.GONE
+        binding.waitingResponseState.visibility = View.VISIBLE
+        binding.loginButtonProfile.visibility = View.GONE
     }
 
     private fun hideProgress() {
-        waitingResponseState.visibility = View.GONE
-        loginButtonProfile.visibility = View.VISIBLE
+        binding.waitingResponseState.visibility = View.GONE
+        binding.loginButtonProfile.visibility = View.VISIBLE
     }
 
     private fun showSnack(message: String) {
